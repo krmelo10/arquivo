dfe6d64e94ae10a8d4c5a4fcf7708a05c8980cc0
Karen Melo <krmelo@unifesp.br>
2025-01-01 21:10:25 -0300

    lista de compras (fila)

---
+++ 24. 2729.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 20
+#define ITEM 1001
+
+typedef struct no{
+  char item[MAX];
+  struct no *seg;
+}no;
+
+//aloca nó.
+no *criano(char item[]){
+  no *novo = malloc(sizeof(no));
+  if(novo != NULL){
+    strcpy(novo->item, item);
+    novo->seg = NULL;
+    return novo;
+  }
+  return novo;
+}
+
+//inserção.
+void insere(no **head, char item[]){
+  no *novo = criano(item);
+  if(*head == NULL){
+    *head = novo;
+    return;
+  }
+  no *aux = *head;
+  while(aux != NULL){
+    if(strcmp(aux->item, item) == 0){
+      return;
+    }
+    aux = aux->seg;
+  }
+  no *temp = *head;
+  while(temp->seg != NULL){
+    temp = temp->seg;
+  }
+  temp->seg = novo;
+}
+
+//(bubble sort) ordenação por ordem alfabética.
+void ordena(no *head){
+  if(head == NULL)
+    return;
+  no *n, *m;
+  char temp[MAX];
+
+  for(n = head; n != NULL; n = n->seg){
+    for(m = n->seg; m != NULL; m = m->seg){
+      if(strcmp(n->item, m->item) > 0){
+        strcpy(temp, n->item);
+        strcpy(n->item, m->item);
+        strcpy(m->item, temp);
+      }
+    }
+  }
+}
+
+//printa a lista
+void imprime(no *head){
+  no *temp = head;
+  while(temp != NULL){
+    printf("%s ", temp->item);
+    temp = temp->seg;
+  }
+  printf("\n");
+}
+
+int main(){
+  int N;
+  scanf("%d", &N);
+
+  for(int n = 0; n < N; n++){
+    no *lista = NULL;
+
+    //leitura dos itens da lista de compras
+    char linha[ITEM];
+    getchar();
+    fgets(linha, ITEM, stdin);
+
+    //divide a linha em palavras e as insere na lista
+    char *item = strtok(linha, " \n");
+    while(item != NULL){
+      insere(&lista, item);
+      item = strtok(NULL, " \n");
+    }
+    ordena(lista);
+    imprime(lista);
+  }
+  return 0;
+}
